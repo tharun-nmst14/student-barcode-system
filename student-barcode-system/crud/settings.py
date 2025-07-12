e6d2b7501cd55817cb87014e704784c63694ffc9
THARUN AZMEERA <138688779+tharun-nmst14@users.noreply.github.com>
2025-07-12 17:53:52 +0530

    Update settings.py

--- student-barcode-system/crud/settings.py
+++ student-barcode-system/crud/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = "django-insecure-!7jwknwz^n8)sf16%vp=oj+jebqztjs)#+19zqe)&ngjdw^7sv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
